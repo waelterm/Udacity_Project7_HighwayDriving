c6e8de4c555831627d250c1681e62b16667029d2
waelterm <waelterm@msu.edu>
2019-09-18 22:29:09 -0700

    parametertuning

--- src/main.cpp
+++ src/main.cpp
@@ -397,7 +397,7 @@ int main() {
 					double ref_accel;
 					double max_accel = 1.5;
 					double max_decel = 1;
-					double max_jerk = 5;
+					double max_jerk = 1;
 					// 3 State Machine - Accelerate, Decelerate, Keep Speed
 					if (no_lane_change_counter != 0) {
 						max_accel *= (1 - no_lane_change_counter / 150.0);
